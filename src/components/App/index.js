b350fbed80fca04bcb565f6a6488d54feeb8a502
Jonathan Lester <jonathanmarklester@gmail.com>
2019-04-04 16:50:47 +0000

    added team scores

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import css from "./App.module.css";
+
+import Score from "../Score";
+
 const gist_ID = "69c73b41c0c513a711c5932e37a55f59";
 const gist_URL = `https://gist.githubusercontent.com/j0nnylester/${gist_ID}/raw`;
 
@@ -8,13 +11,26 @@ class App extends Component {
         super(props);
         this.state = {
             bootcampers: [],
-            bootcamper: "",
+            bootcamper: "Player",
             facts: [],
             fact: "",
             isRevealed: false,
-            reveal: "True"
+            reveal: "True",
+            vsTeam: 0,
+            one: 0,
+            two: 0,
+            three: 0,
+            four: 0,
+            five: 0,
+            six: 0
         };
     }
+
+    changeScore = (team, change) => {
+        this.setState(state => ({
+            [team]: state[team] + change
+        }));
+    };
 
     reveal = () => {
         this.setState(() => ({ isRevealed: true }));
@@ -30,14 +46,15 @@ class App extends Component {
             Math.floor(Math.random() * this.state.facts.length)
         ];
         if (rfact.reveal === "True" && rfact.who === rbootcamper) {
-            this.setState(state => ({
+            this.setState(() => ({
                 fact: rfact,
                 bootcamper: rbootcamper
             }));
         } else {
-            this.setState(state => ({
+            this.setState(() => ({
                 fact: "",
-                bootcamper: ""
+                bootcamper: "",
+                vsTeam: Math.floor(Math.random() * 5) + 1
             }));
         }
     };
@@ -62,7 +79,11 @@ class App extends Component {
             <div className={css.app}>
                 <header className={css.appheader}>
                     <h1 className={css.title}>Would I Lie To You?</h1>
-                    <h3 className={css.bootcamper}>{this.state.bootcamper}</h3>
+                    <h3 className={css.bootcamper}>
+                        {`${this.state.bootcamper} vs team: ${
+                            this.state.vsTeam
+                        }`}{" "}
+                    </h3>
                     <p className={css.fact}>
                         {this.state.fact ? `"${this.state.fact.fact}"` : ""}
                     </p>
@@ -75,6 +96,38 @@ class App extends Component {
                     )}
                     <div className={css.randomise} onClick={this.randomise}>
                         Play
+                    </div>
+                    <div className={css.scores}>
+                        <Score
+                            team="one"
+                            score={this.state.one}
+                            changeScore={this.changeScore}
+                        />
+                        <Score
+                            team="two"
+                            score={this.state.two}
+                            changeScore={this.changeScore}
+                        />
+                        <Score
+                            team="three"
+                            score={this.state.three}
+                            changeScore={this.changeScore}
+                        />
+                        <Score
+                            team="four"
+                            score={this.state.four}
+                            changeScore={this.changeScore}
+                        />
+                        <Score
+                            team="five"
+                            score={this.state.five}
+                            changeScore={this.changeScore}
+                        />
+                        <Score
+                            team="six"
+                            score={this.state.six}
+                            changeScore={this.changeScore}
+                        />
                     </div>
                 </header>
             </div>
