d75244c39bd96fc3993542746ce5b8ceeccbf95a
Jonathan Lester <jonathanmarklester@gmail.com>
2019-04-29 19:20:50 +0000

    Updates

---
+++ precache-manifest.f31274d3046134425b7b3f7b08fa8ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3be7057fb7387acbebb3",
+    "url": "/wilty/static/js/runtime~main.ab430a59.js"
+  },
+  {
+    "revision": "786aec54336bbb2c1788",
+    "url": "/wilty/static/js/main.04a80f78.chunk.js"
+  },
+  {
+    "revision": "a6d630e3430ce538b34c",
+    "url": "/wilty/static/js/2.3837ad9c.chunk.js"
+  },
+  {
+    "revision": "786aec54336bbb2c1788",
+    "url": "/wilty/static/css/main.bb72c437.chunk.css"
+  },
+  {
+    "revision": "d60113ef9d2351f3be96e79d3ca19650",
+    "url": "/wilty/index.html"
+  }
+];
